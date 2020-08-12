a6fa269f6ffdfc928a5aa81321338db32b26f825
川岡優太 <kawaokayuta@vinaka.local>
2020-08-12 16:09:07 +0900

    fix

--- blog.py
+++ blog.py
@@ -1,11 +1,11 @@
 class Blog:
 
     def write_blog(self):
-        print("日付を入力してください")
+        print(“日付を入力してください”)
         self.date = input()
-        print("タイトルを入力してください”)
+        print(“タイトルを入力してください”)
         self.title = input()
-        print("本文を入力してください”)
+        print(“本文を入力してください”)
         self.contents = input()
 
     def show_blog(self):
